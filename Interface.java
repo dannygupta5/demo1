83db088d1bee33e716fb823e9de62689f9dc9a1a
dannygupta5 <dannygupta.cse22@jecrc.ac.in>
2022-04-27 15:13:29 +0530

    modified interface and Faculty

--- Interface.java
+++ Interface.java
@@ -1,19 +1,55 @@
-public interface Interface {
+interface Faculty {
 
-    public void Display();
+   
+
+    public String name(String name);
+
+    public int age(int age);
+
 }
 
-class example implements Interface {
+class Teacher implements Faculty {
 
     @Override
-    public void Display() {
-        System.out.println("This is a implemented class");
-
+    public String name(String name) {
+        return name;
     }
 
-    public static void main(String args[]) {
-        example ob = new example();
-        ob.Display();
-    }
+    @Override
+    public int age(int age) {
 
+        return age;
+    }
 }
+    class Student implements Faculty {
+
+        @Override
+        public String name(String name) {
+
+            return name;
+        }
+
+        @Override
+        public int age(int age) {
+
+            return age;
+        }
+    }
+
+public class Interface {
+    
+    public static void main(String args[]) {
+        
+    
+            Faculty ob1 = new Teacher();
+
+            ob1.name("Danny");
+            ob1.age(29);
+
+            Faculty ob2 = new Student();
+
+            ob2.name("Rahul");
+            ob2.age(23);
+        }
+    }
+

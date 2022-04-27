0e8f551d20cd0eb8fc4d92842f4fee773b04f464
dannygupta5 <dannygupta.cse22@jecrc.ac.in>
2022-04-27 15:30:34 +0530

    deleted interface and Faculty

---
+++ Collection.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Collection {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+
+        list.add(13);
+        list.add(39);
+        list.add(87);
+
+        System.out.println(list);
+
+    }
+}
